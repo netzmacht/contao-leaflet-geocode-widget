29acdb62d849d52cafde8668318207030a83609e
David Molineus <david.molineus@netzmacht.de>
2017-01-18 17:24:24 +0100

    Complete english translation.

--- src/Resources/contao/languages/en/default.php
+++ src/Resources/contao/languages/en/default.php
@@ -8,4 +8,6 @@
  *
  */
 
-$GLOBALS['TL_LANG']['ERR']['leaflet_invalid_coordinate'] = 'Invalid coordinates given.';
+$GLOBALS['TL_LANG']['ERR']['leafletInvalidCoordinate']   = 'Invalid coordinates given.';
+$GLOBALS['TL_LANG']['MSC']['leafletSearchPositionLabel'] = 'Search';
+$GLOBALS['TL_LANG']['MSC']['leafletApplyPositionLabel']  = 'Apply';
