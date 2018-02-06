c0ce2f4c5ce1799873d1fbc2595a172296addf2e
David Molineus <david.molineus@netzmacht.de>
2018-02-06 13:19:05 +0100

    Move GeocodeWidget to Widget namespace.

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -5,9 +5,9 @@
  *
  * @package    netzmacht
  * @author     David Molineus <david.molineus@netzmacht.de>
- * @copyright  2016-2017 netzmacht David Molineus. All rights reserved.
+ * @copyright  2016-2018 netzmacht David Molineus. All rights reserved.
  * @license    LGPL-3.0 https://github.com/netzmacht/contao-leaflet-geocode-widget/blob/master/LICENSE
  * @filesource
  */
 
-$GLOBALS['BE_FFL']['leaflet_geocode'] = 'Netzmacht\Contao\Leaflet\GeocodeWidget\GeocodeWidget';
+$GLOBALS['BE_FFL']['leaflet_geocode'] = 'Netzmacht\Contao\Leaflet\GeocodeWidget\Widget\GeocodeWidget';
